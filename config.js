6564b2942f71f34ef3ec7ce192337fc848c508d1
Successtega <164101384+Successtega@users.noreply.github.com>
2024-03-26 09:10:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349028918516', 'GURU', true],
+  ['2349028918516', 'Ash', true], 
   [''] 
 ] //Number of owners
 
